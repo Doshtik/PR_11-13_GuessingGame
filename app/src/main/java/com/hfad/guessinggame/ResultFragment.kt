91df63fa456ce3580f8a72533ec001216fb71522
Doshtik <igor.lizyakinvladislavovic@gmail.com>
2025-02-17 11:54:40 +0500

    The End of PR_11

--- app/src/main/java/com/hfad/guessinggame/ResultFragment.kt
+++ app/src/main/java/com/hfad/guessinggame/ResultFragment.kt
@@ -5,19 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.hfad.guessinggame.databinding.FragmentResultBinding
 import androidx.navigation.findNavController
 
 class ResultFragment : Fragment() {
     private var _binding: FragmentResultBinding? = null
     private val binding get() = _binding!!
+    lateinit var viewModel: ResultViewModel
+    lateinit var viewModelFactory: ResultViewModelFactory
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         _binding= FragmentResultBinding.inflate(inflater, container, false)
         val view = binding.root
 
-        binding.wonLost.text = ResultFragmentArgs.fromBundle(requireArguments()).result
+        val result = ResultFragmentArgs.fromBundle(requireArguments()).result
+        viewModelFactory = ResultViewModelFactory(result)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(ResultViewModel::class.java)
+
+        binding.wonLost.text = viewModel.result
 
         binding.newGameButton.setOnClickListener() {
             view.findNavController()
