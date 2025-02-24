ae5fbfb237ed86013fed2958c83b618f6b203755
Doshtik <igor.lizyakinvladislavovic@gmail.com>
2025-02-24 09:28:18 +0500

    PR_12, page 529

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
--- app/src/main/java/com/hfad/guessinggame/GameViewModel.kt
+++ app/src/main/java/com/hfad/guessinggame/GameViewModel.kt
@@ -1,16 +1,17 @@
 package com.hfad.guessinggame
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.MutableLiveData
 
 class GameViewModel: ViewModel() {
     val words = listOf("Android", "Activity", "Fragment")
     val secretWord = words.random().uppercase()
-    var secretWordDisplay = ""
+    val secretWordDisplay = MutableLiveData<String>()
     var correctGuesses = ""
-    var incorrectGuesses = ""
-    var livesLeft = 8
+    val incorrectGuesses = MutableLiveData<String>("")
+    val livesLeft = MutableLiveData<Int>(8)
     init {
-        secretWordDisplay = deriveSecretWordDisplay()
+        secretWordDisplay.value = deriveSecretWordDisplay()
     }
     fun deriveSecretWordDisplay() : String {
         var display = ""
@@ -28,15 +29,15 @@ class GameViewModel: ViewModel() {
         if (guess.length == 1) {
             if (secretWord.contains(guess)) {
                 correctGuesses += guess
-                secretWordDisplay = deriveSecretWordDisplay()
+                secretWordDisplay.value = deriveSecretWordDisplay()
             } else {
-                incorrectGuesses += "$guess "
-                livesLeft--
+                incorrectGuesses.value += "$guess "
+                livesLeft.value = livesLeft.value?.minus(1)
             }
         }
     }
-    fun isWon() = secretWord.equals(secretWordDisplay, true)
-    fun isLost() = livesLeft <= 0
+    fun isWon() = secretWord.equals(secretWordDisplay.value, true)
+    fun isLost() = livesLeft.value ?: 0 <= 0
     fun wonLostMessage() : String {
         var message = ""
         if (isWon()) message = "You won!"
